b5502dbbd2b623597866588e45d6f2ad06940535
kovus380 <kosaf888@gmail.com>
2021-08-21 00:14:59 +0900

    2021/08/20

--- BOJ/siv_1/2231/2231_sol_not_use_bool.py
+++ BOJ/siv_1/2231/2231_sol_not_use_bool.py
@@ -2,7 +2,11 @@ N: int = int(input())
 
 tag: bool = False
 
-for i in range(1, N+1):
+# 각 자리에서 가질 수 있는 가장 큰 값은 9
+# 수 + 수의 각 자리 합 = 분해합이므로
+# 가장 작은 수(범위를 지정할)는 분해합 - 수의 각 자리 합의 최대값
+# 즉 N-9*6
+for i in range(max(1, N-54), N+1):
     if i + sum(map(int, list(str(i)))) == N:
         print(i)
         tag = True
