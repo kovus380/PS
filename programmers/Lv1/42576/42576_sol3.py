4f8ab0cccc9f86959eefd26528bf63f281e38258
kovus380 <86646679+kovus380@users.noreply.github.com>
2022-01-05 00:50:23 +0900

    2022/01/05

--- programmers/Lv1/42576/42576_sol3.py
+++ programmers/Lv1/42576/42576_sol3.py
@@ -7,6 +7,8 @@ def solution(participant, completion):
         if participant[i] != completion[i]:
             return participant[i]
     return participant[-1]
+
+
 participant = ["mislav", "stanko", "mislav", "ana"]
 completion = ["stanko", "ana", "mislav"]
 res = solution(participant, completion)
