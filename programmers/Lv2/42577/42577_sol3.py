ab4e8137db584218ad223e13f3ac391f2fd91b2b
kovus380 <kosaf888@gmail.com>
2022-01-08 15:32:27 +0900

    2022/01/08

--- programmers/Lv2/42577/42577_sol3.py
+++ programmers/Lv2/42577/42577_sol3.py
@@ -1,11 +1,16 @@
 # 42577 | 전화번호 목록
 # https://programmers.co.kr/learn/courses/30/lessons/42577
 def solution(phone_book):
-    answer = True
     hash_map = {}
-    for phone
-    return answer
-
+    for phone_number in phone_book:
+        hash_map[phone_number] = 1
+    for phone_number in phone_book:
+        temp = ""
+        for number in phone_number:
+            temp += number
+            if temp in hash_map and temp != phone_number:
+                return False
+    return True
 phone_book = ["12","123","1235","567","88"]
 res = solution(phone_book)
 print(res)
